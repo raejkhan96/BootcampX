03063f1cf68fde7230b81b5af8c77cd2474cbfcf
Rae J. Khan <raekhan18@gmail.com>
2021-09-14 20:11:30 +0000

    Queries 4

---
+++ 4_queries/cohortLongestAssistance.sql
@@ -0,0 +1,9 @@
+SELECT cohorts.name AS name, AVG(completed_at - started_at) AS average_assistance_request_duration
+FROM assistance_requests
+JOIN students
+ON students.id = assistance_requests.student_id
+JOIN cohorts
+ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_request_duration DESC
+LIMIT 1;
---
+++ 4_queries/totalStudentAssistanceRequests.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(assistance_requests.student_id) AS total_assistances, students.name AS name
+FROM students
+JOIN assistance_requests
+ON students.id = assistance_requests.student_id
+WHERE students.name = 'Elliot Dickinson'
+GROUP BY students.name;
